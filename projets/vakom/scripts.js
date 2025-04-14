f2e89dc2edcf53f73a5089fafc6ea0f9acdedaa0
Thomas Trapani <thomala13@gmail.com>
2025-04-14 14:54:46 +0200

    Update scripts.js

--- projets/vakom/scripts.js
+++ projets/vakom/scripts.js
@@ -9,3 +9,15 @@ window.addEventListener("scroll", function() {
     navbar.classList.remove("sticky");
   }
 });
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+// Ajoute une barre centrale au hamburger
+const bar = document.createElement("div");
+hamburger.appendChild(bar);
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
